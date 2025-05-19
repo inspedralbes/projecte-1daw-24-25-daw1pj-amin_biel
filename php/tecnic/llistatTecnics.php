75fa2d9e0a34c2222d8dd56443b681cd0ba85554
a24biedommar <a24biedommar@inspedralbes.cat>
2025-05-19 08:59:41 +0200

    arreglat

--- php/tecnic/llistatTecnics.php
+++ php/tecnic/llistatTecnics.php
@@ -16,7 +16,7 @@ insertLogs($collection, $Usuari, $data, $ipUsuari ,$paginaUsuari);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Llistat Tècnics - Amin&Biel/title>
+    <title>Llistat Tècnics - Amin&Biel</title>
     <link rel="icon" type="image/x-icon" href="../img/LogoInsp.jpg">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
